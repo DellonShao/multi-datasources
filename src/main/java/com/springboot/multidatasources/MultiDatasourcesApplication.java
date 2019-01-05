46f13a41e270baa9edd8d850370de4eaba5cceca
DellonShao <14301102@bjtu.edu.cn>
2019-01-05 21:04:28 +0800

    initial

---
+++ src/main/java/com/springboot/multidatasources/MultiDatasourcesApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.multidatasources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiDatasourcesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiDatasourcesApplication.class, args);
+    }
+
+}
+
